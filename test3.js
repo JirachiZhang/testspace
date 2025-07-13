866dfb7dd6edb13766bc15668d907fbfa3228da8
JirachiZhang <jirachi.zhang@foxmail.com>
2025-07-13 21:32:46 +0800

    Create test3.js

---
+++ test3.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         test3
+// @namespace    http://tampermonkey.net/
+// @version      2025-07-09
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @require      https://unpkg.com/babel/parser@7.28.0/lib/index.js
+// @require      https://unpkg.com/babel/traverse@7.28.0/lib/index.js
+// @require      https://unpkg.com/axios@1.3.6/dist/axios.min.js
+// @require      https://unpkg.com/store.js@1.0.4/store.js
+// @require      https://unpkg.com/jquery@3.6.4/dist/jquery.min.js
+// @require      https://unpkg.com/jszip@3.1.5/dist/jszip.min.js
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @require      https://unpkg.com/crypto-js@4.1.1/crypto-js.js
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @grant        GM_xmlhttpRequest
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const url = 'https://hi77-overseas.mangafuna.xyz/static/websitefree/js20190704/comic_detail_pass202507081916.js';
+    let astTree;
+
+    axios.get(url)
+            .then((response) => {
+        astTree = parser.parse(response);
+    });
+
+    // AST代码
+    let visitor = {
+        StringLiteral({node}) {
+            if (node.value !== node.extra.raw) {
+                node.extra.raw = "'" + node.value + "'";
+            }
+        },
+    }
+    traverse(astTree, visitor);
+
+
+
+
+
+})();
